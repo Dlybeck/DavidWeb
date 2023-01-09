c51356f58a6f6deeb848df2b3c509e66a396461e
dlybeck383 <dlybeck@pugetsound.edu>
2023-01-08 18:53:06 -0800

    Made transitions way simpler and fixed lag in the process

--- public/scripts/transitions.js
+++ public/scripts/transitions.js
@@ -1,14 +1,16 @@
-let transTime = 250;
-let homeX = 0;
-let homeY = 0;
+const fadeTime = 250;
+const fadeTimeS = '0.25s';
+const moveTime = 2000;
+const moveTimeS = '2s';
 
 window.onload = () => {
     const transition_el = document.querySelector('.transition');
     const anchors = document.querySelectorAll('a');
 
     setTimeout(() => {
+        //set transition state
         transition_el.classList.remove('is-active');
-    }, transTime);
+    }, fadeTime);
 
     for(let i=0; i<anchors.length; i++){
         const anchor = anchors[i]
@@ -16,104 +18,27 @@ window.onload = () => {
         anchor.addEventListener('click', e=>{
             e.preventDefault();
             let target = e.target.href;
-
+            //set transition state
             transition_el.classList.add('is-active');
 
-            //from home to destination
-            if(transition_el.classList.contains("transition-Home")){
-                let id = null;
-                let x = 0;
-                let y = 0;
-                let destX = e.target.getAttribute('x');
-                let destY = e.target.getAttribute('y');
+            //if it is a transition
+            if(transition_el.classList.contains("transition-page")){
+                let x = e.target.getAttribute('x')
+                let y = e.target.getAttribute('y')
+                //wait for fade to happen then start moving background
+                setTimeout(() => {
+                    //change to movement trans time
+                    transition_el.style.transition = "ease-in-out " + moveTimeS;
+                    transition_el.style.backgroundPosition = x + "px " + y + "px"
 
-                clearInterval(id);
-                id = setInterval(move, 1);
-                function move(){
-                    if(x==destX && y==destY){
-                        clearInterval(id);
-                        finish();
-                    }
-                    else{
-                        if(x!=destX){
-                            if(destX > x){x+=5}
-                            else{x-=5}
-                        }
-                        if(y!=destY){
-                            if(destY > y){y+=5}
-                            else{y-=5}
-                        }
-                        transition_el.style.backgroundPosition = x+"px "+ y+"px";
-                        
-                    }
-                }
-                function finish(){
+                    //wait for background movement to happen then start changing pages
                     setTimeout(() => {
+                        //return to fade trans time
+                        transition_el.style.transition = "ease-out " + fadeTimeS;
+
                         window.location.href = target;
-                    }, transTime);
-                }
-            } 
-            //from tic tac toe to home
-            if(transition_el.classList.contains("transition-1")){
-                let id = null;
-                let x = -2000;
-                let y = -2000;
-                clearInterval(id);
-                id = setInterval(move, 1);
-                function move(){
-                    if(x==homeX && y==homeY){
-                        clearInterval(id);
-                        finish();
-                    }
-                    else{
-                        if(x!=homeX){
-                            if(homeX > x){x+=5}
-                            else{x-=5}
-                        }
-                        if(y!=homeY){
-                            if(homeY > y){y+=5}
-                            else{y-=5}
-                        }
-                        transition_el.style.backgroundPosition = x+"px "+ y+"px";
-                        
-                    }
-                }
-                function finish(){
-                    setTimeout(() => {
-                        window.location.href = target;
-                    }, transTime);
-                }
-            } 
-            //from test page to home
-            if(transition_el.classList.contains("transition-2")){
-                let id = null;
-                let x = 2000;
-                let y = 2000;
-                clearInterval(id);
-                id = setInterval(move, 1);
-                function move(){
-                    if(x==homeX && y==homeY){
-                        clearInterval(id);
-                        finish();
-                    }
-                    else{
-                        if(x!=homeX){
-                            if(homeX > x){x+=5}
-                            else{x-=5}
-                        }
-                        if(y!=homeY){
-                            if(homeY > y){y+=5}
-                            else{y-=5}
-                        }
-                        transition_el.style.backgroundPosition = x+"px "+ y+"px";
-                        
-                    }
-                }
-                function finish(){
-                    setTimeout(() => {
-                        window.location.href = target;
-                    }, transTime);
-                }
+                    }, moveTime);
+                }, fadeTime);
             } 
         });
     }

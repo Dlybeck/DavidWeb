109806878b4fcc0b848fb55ae1f5945de38f0561
dlybeck383 <dlybeck@pugetsound.edu>
2023-01-14 00:37:23 -0800

    Menu and main page done?

--- public/scripts/transitions.js
+++ public/scripts/transitions.js
@@ -5,7 +5,7 @@ const moveTimeS = '2s';
 
 window.onload = () => {
     var pos = window.getComputedStyle(document.body).getPropertyValue('background-position');
-    console.log(pos)
+    //console.log(pos)
     const transition_el = document.querySelector('.transition');
     const anchors = document.querySelectorAll('[link=true]');
 
@@ -20,7 +20,7 @@ window.onload = () => {
         const anchor = anchors[i]
 
         anchor.addEventListener('click', e=>{
-            console.log(e.target.getAttribute('href'))
+            //console.log(e.target.getAttribute('href'))
             e.preventDefault();
             let target = e.target.getAttribute('href');
             //set transition state
@@ -30,7 +30,7 @@ window.onload = () => {
             if(transition_el.classList.contains("transition-page")){
                 let x = e.target.getAttribute('x')
                 let y = e.target.getAttribute('y')
-                console.log("going to (" + x + ", " + y + ")")
+                //console.log("going to (" + x + ", " + y + ")")
                 //wait for fade to happen then start moving background
                 setTimeout(() => {
                     //change to movement trans time

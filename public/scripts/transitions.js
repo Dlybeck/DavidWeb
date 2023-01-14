91927c8b04d0e64e520de0880c15effeb502f79c
dlybeck383 <dlybeck@pugetsound.edu>
2023-01-13 21:30:03 -0800

    Most transitions working

--- public/scripts/transitions.js
+++ public/scripts/transitions.js
@@ -4,20 +4,25 @@ const moveTime = 2000;
 const moveTimeS = '2s';
 
 window.onload = () => {
+    var pos = window.getComputedStyle(document.body).getPropertyValue('background-position');
+    console.log(pos)
     const transition_el = document.querySelector('.transition');
-    const anchors = document.querySelectorAll('a');
+    const anchors = document.querySelectorAll('[link=true]');
 
     setTimeout(() => {
         //set transition state
         transition_el.classList.remove('is-active');
     }, fadeTime);
 
+    var pos = window.getComputedStyle(document.body).getPropertyValue('background-position');
+
     for(let i=0; i<anchors.length; i++){
         const anchor = anchors[i]
 
         anchor.addEventListener('click', e=>{
+            console.log(e.target.getAttribute('href'))
             e.preventDefault();
-            let target = e.target.href;
+            let target = e.target.getAttribute('href');
             //set transition state
             transition_el.classList.add('is-active');
 
@@ -25,6 +30,7 @@ window.onload = () => {
             if(transition_el.classList.contains("transition-page")){
                 let x = e.target.getAttribute('x')
                 let y = e.target.getAttribute('y')
+                console.log("going to (" + x + ", " + y + ")")
                 //wait for fade to happen then start moving background
                 setTimeout(() => {
                     //change to movement trans time

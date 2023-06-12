77ab42f125d7c5c2cbc1d64883d888b4aacb78d2
dlybeck <dlybeck383@gmail.com>
2023-06-12 11:45:03 -0700

    Canvas 'map' saving across pages?

--- public/scripts/transitions.js
+++ public/scripts/transitions.js
@@ -7,8 +7,17 @@ window.onload = () => {
     const transition_el = document.querySelector('.transition');
     const anchors = document.querySelectorAll('[link=true]');
 
-    checkCoords(document.body, "body");
-    checkCoords(transition_el, "transition_el");
+    let coords = checkCoords(document.body, "body");
+    printCoords(coords, "body")
+    coords = checkCoords(transition_el);
+    printCoords(coords, "transition_el");
+
+
+    const canvas = document.querySelectorAll('#canvas');
+
+    console.log("deserializing")
+    deserializeCanvas(canvas[0])
+    console.log("deserialized")
 
     setTimeout(() => {
         //set transition state
@@ -25,8 +34,16 @@ window.onload = () => {
             //set transition state
             transition_el.classList.add('is-active');
 
+
             //if it is a transition
             if(transition_el.classList.contains("transition-page")){
+
+                const ctx = canvas[0].getContext('2d');
+                drawLine(ctx, [100, 100], [100, 300], 'green', 5);
+                console.log("Drew Line?")
+
+                serializeCanvas(canvas[0])
+
                 let x = e.target.getAttribute('x')
                 let y = e.target.getAttribute('y')
                 console.log("going to (" + x + ", " + y + ")")
@@ -49,6 +66,40 @@ window.onload = () => {
     }
 }
 
-function checkCoords(element, elName){
-    console.log("Current position of " + elName + "is " + window.getComputedStyle(element).getPropertyValue('background-position'))
+function drawLine(ctx, begin, end, stroke = 'black', width = 10) {
+    if (stroke) {
+        ctx.strokeStyle = stroke;
+    }
+
+    if (width) {
+        ctx.lineWidth = width;
+    }
+
+    ctx.beginPath();
+    ctx.moveTo(...begin);
+    ctx.lineTo(...end);
+    ctx.stroke();
+}
+
+function checkCoords(element){
+    return(window.getComputedStyle(element).getPropertyValue('background-position').split(/\s+/));
+
+}
+
+function printCoords(coords, elName){
+    console.log(elName +  " is currently at " + coords);
+}
+
+function serializeCanvas(canvas) {
+    localStorage.setItem('map', canvas.toDataURL());
+}
+
+function deserializeCanvas(canvas) {
+    let data = localStorage.getItem('map');
+    var img = new Image();
+    img.onload = function() {
+        canvas.getContext("2d").drawImage(img, 0, 0);
+    };
+
+    img.src = data;
 }
